eb2df9970ee1b7051b77b91c0b04092caf3de867
Kgodiso-Leboho <kgodisoaustinleboho@gmail.com>
2026-04-24 15:05:17 +0200

    Fixed the CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,14 @@ from app.rag.pipeline import ask
 
 app = FastAPI()
 
+origins = [
+    "https://my-portfolio-git-main-kgodisos-projects.vercel.app",
+    "http://localhost:5173",  # optional for local dev
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
